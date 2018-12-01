c86fb1aeb5f532923dae35797aef9fe59274b131
aaa <1289181391@qq.com>
2018-12-01 17:56:50 +0800

    更换了新包

--- app/src/main/java/com/example/BottomNavFragmentThree.kt
+++ app/src/main/java/com/example/BottomNavFragmentThree.kt
@@ -2,12 +2,14 @@ package com.example
 
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class BottomNavFragmentThree : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:
-    Bundle?): View? = inflater.inflate(R.layout.fragment_bottom_nav_fragment_three, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:
+        Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_bottom_nav_fragment_three, container, false)
 }
--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -1,23 +1,24 @@
 package com.example
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navController = Navigation.findNavController(this, R.id.mainNavFragment)
+        navController = Navigation.findNavController(this, R.id.mainNavFragment)
         setSupportActionBar(toolbar)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         navigationView.setupWithNavController(navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean = NavigationUI.navigateUp(drawerLayout,
-            Navigation.findNavController(this, R.id.mainNavFragment))
+    override fun onSupportNavigateUp(): Boolean = NavigationUI.navigateUp(navController, drawerLayout)
 }
